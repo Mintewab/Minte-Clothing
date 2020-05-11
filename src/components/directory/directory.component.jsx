9e3a58c6e65cea860b5b739320dd46a0e1fc9677
Mintewab <demissiewminte123@gmail.com>
2020-05-11 16:45:30 -0700

    routing the hats page

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -14,29 +14,34 @@ class Directory extends React.Component {
         {
           title: 'hats',
           imageUrl: 'https://i.pinimg.com/originals/0b/b9/d2/0bb9d27d5de16cabe820a3608952a7ef.jpg',
-          id: 1
+          id: 1,
+          linkUrl: 'hats'
         },
         {
           title: 'jackets',
           imageUrl: 'https://image-cdn.hypb.st/https%3A%2F%2Fhypebeast.com%2Fimage%2F2017%2F11%2Fcanada-goose-london-regent-street-store-opening-6.jpg?quality=95&w=1170&cbr=1&q=90&fit=max',
-          id: 2
+          id: 2,
+          linkUrl: ''
         },
         {
           title: 'sneakers',
           imageUrl: 'https://arch-usa.com/wp-content/uploads/2017/10/uploads2F1508887017577-AFEWxSAUCONY-Studio-1200x800-1.jpg',
-          id: 3
+          id: 3,
+          linkUrl: ''
         },
         {
           title: 'womens',
           imageUrl: 'https://i.pinimg.com/originals/e8/d4/5f/e8d45f5a041948369a3369e507da9102.jpg',
           size: 'large',
-          id: 4
+          id: 4,
+          linkUrl: ''
         },
         {
           title: 'mens',
           imageUrl: 'https://www.thefashionisto.com/wp-content/uploads/2018/11/Mango-Man-Holiday-2018-Campaign-003.jpg',
           size: 'large',
-          id: 5
+          id: 5,
+          linkUrl: ''
         }
       ]
     };
@@ -45,8 +50,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className='directory-menu'>
-        {this.state.sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
     );
